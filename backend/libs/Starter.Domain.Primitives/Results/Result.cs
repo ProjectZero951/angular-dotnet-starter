ae54602d4f7b0e61432b05edae4043dae126090c
Krzysztof Torończak <k.toronczak.dev@gmail.com>
2026-02-07 13:06:39 +0100

    refactor: replace Expection type parameter in Result.Fail method with Error Type & implement Error

--- backend/libs/Starter.Domain.Primitives/Results/Result.cs
+++ backend/libs/Starter.Domain.Primitives/Results/Result.cs
@@ -1,27 +1,28 @@
 ﻿namespace Starter.Domain.Primitives.Results;
 
+public record Error(string Message, string? Code = null);
+
 public class Result
 {
     public bool IsSuccess { get; }
     public bool IsFailure => !IsSuccess;
+    public Error? Error { get; }
 
-    public Exception? DomainError { get; }
-
-    protected Result(bool success, Exception? domainError)
+    protected Result(bool success, Error? error)
     {
         IsSuccess = success;
-        DomainError = domainError;
+        Error = error;
     }
 
     public static Result Ok() => new(true, null);
-    public static Result Fail(Exception error) => new(false, error);
+    public static Result Fail(Error error) => new(false, error);
 }
 
 public class Result<T> : Result
 {
     public T? Value { get; }
 
-    private Result(bool success, T? value, Exception? domainError)
+    private Result(bool success, T value, Error? domainError)
         : base(success, domainError)
     {
         Value = value;
@@ -29,5 +30,5 @@ public class Result<T> : Result
 
     public static Result<T> Ok(T value) => new(true, value, null);
 
-    public new static Result<T> Fail(Exception error) => new(false, default, error);
+    public new static Result<T> Fail(Error error) => new(false, default!, error);
 }
